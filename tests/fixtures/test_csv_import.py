0ee5d4382a43ef041fe87ef8da748b3362594350
rcombresansq-pixel <rcombresansq@gmail.com>
2026-09-25 16:11:20 +0200

    Adding new CSV test files

--- tests/fixtures/test_csv_import.py
+++ tests/fixtures/test_csv_import.py
@@ -1,6 +1,6 @@
 import os
 import csv
-from api import FastAPI
+#from api import FastAPI
 
 with open('01_valide.csv', "r", encoding="utf-8") as fichier:
     reader = csv.DictReader(fichier)
@@ -8,6 +8,32 @@ with open('01_valide.csv', "r", encoding="utf-8") as fichier:
         print(ligne)
 
 with open('02_colonne_manquante.csv', "r", encoding="utf-8") as fichier:
+    reader = csv.DictReader(fichier)
+    for ligne in reader:
+        print(ligne)
+
+with open('03_colonnes_en_plus.csv', "r", encoding="utf-8") as fichier:
+    reader = csv.DictReader(fichier)
+    for ligne in reader:
+        print(ligne)
+
+
+with open('04_lignes_mixtes.csv', "r", encoding="utf-8") as fichier:
+    reader = csv.DictReader(fichier)
+    for ligne in reader:
+        print(ligne)
+
+with open('05_doublons_internes.csv', "r", encoding="utf-8") as fichier:
+    reader = csv.DictReader(fichier)
+    for ligne in reader:
+        print(ligne)
+
+with open('06_premier_invalide.csv', "r", encoding="utf-8") as fichier:
+    reader = csv.DictReader(fichier)
+    for ligne in reader:
+        print(ligne)
+
+with open('07_reimport.csv', "r", encoding="utf-8") as fichier:
     reader = csv.DictReader(fichier)
     for ligne in reader:
         print(ligne)
