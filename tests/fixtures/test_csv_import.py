a173d14d2d70eb16cb97e68e3e3df37f6d9c5fb1
rcombresansq-pixel <rcombresansq@gmail.com>
2026-09-25 16:27:25 +0200

    Adding UTF-8 SIG

--- tests/fixtures/test_csv_import.py
+++ tests/fixtures/test_csv_import.py
@@ -2,43 +2,43 @@ import os
 import csv
 #from api import FastAPI
 
-with open('01_valide.csv', "r", encoding="utf-8") as fichier:
+with open('01_valide.csv', "r", encoding="utf-8-sig") as fichier:
     reader = csv.DictReader(fichier)
     for ligne in reader:
         print(ligne)
 
-with open('02_colonne_manquante.csv', "r", encoding="utf-8") as fichier:
+with open('02_colonne_manquante.csv', "r", encoding="utf-8-sig") as fichier:
     reader = csv.DictReader(fichier)
     for ligne in reader:
         print(ligne)
 
-with open('03_colonnes_en_plus.csv', "r", encoding="utf-8") as fichier:
+with open('03_colonnes_en_plus.csv', "r", encoding="utf-8-sig") as fichier:
     reader = csv.DictReader(fichier)
     for ligne in reader:
         print(ligne)
 
 
-with open('04_lignes_mixtes.csv', "r", encoding="utf-8") as fichier:
+with open('04_lignes_mixtes.csv', "r", encoding="utf-8-sig") as fichier:
     reader = csv.DictReader(fichier)
     for ligne in reader:
         print(ligne)
 
-with open('05_doublons_internes.csv', "r", encoding="utf-8") as fichier:
+with open('05_doublons_internes.csv', "r", encoding="utf-8-sig") as fichier:
     reader = csv.DictReader(fichier)
     for ligne in reader:
         print(ligne)
 
-with open('06_premier_invalide.csv', "r", encoding="utf-8") as fichier:
+with open('06_premier_invalide.csv', "r", encoding="utf-8-sig") as fichier:
     reader = csv.DictReader(fichier)
     for ligne in reader:
         print(ligne)
 
-with open('07_reimport.csv', "r", encoding="utf-8") as fichier:
+with open('07_reimport.csv', "r", encoding="utf-8-sig") as fichier:
     reader = csv.DictReader(fichier)
     for ligne in reader:
         print(ligne)
 
-with open('08_bom.csv', "r", encoding="utf-8 bom") as fichier:
+with open('08_bom.csv', "r", encoding="utf-8-sig") as fichier:
     reader = csv.DictReader(fichier)
     for ligne in reader:
         print(ligne)
